8fa986b3b2d85a2868730180922ea089894b4aef
HarithaMihimal <harithamihimal4@gmail.com>
2026-06-13 15:25:08 +0530

    update Hello.java to demonstrate various data types in Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,21 @@
 class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int num = 9;
+        byte b = (byte) num;
+        System.out.println(b);
+        byte c = 127;
+        System.out.println(c);
+        short sh = 558;
+        System.out.println(sh);
+        long l = 5674l;
+        System.out.println(l);
+        float f = 3.14f;
+        System.out.println(f);
+        double d = 3.14;
+        System.out.println(d);
+        char cc = 'k';
+        System.out.println(cc);
+        boolean bool = true;
+        System.out.println(bool);
     }
 }
