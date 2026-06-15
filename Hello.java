18cb5177353dccbfc471bd0bb471dcd72b3fc586
HarithaMihimal <harithamihimal4@gmail.com>
2026-06-15 07:16:05 +0530

    update Hello.java to demonstrate if-else statements and logical comparisons in Java

--- Hello.java
+++ Hello.java
@@ -1,41 +1,36 @@
 class Hello{
     public static void main(String[] args) {
-     
-    
-        System.out.println("Arithmetic Operators ");
-        int num1 = 10;
-        int num2 = 5;
-        System.out.println("Addition: " + (num1 + num2));
-        System.out.println("Subtraction: " + (num1 - num2));    
-        System.out.println("Multiplication: " + (num1 * num2));
-        System.out.println("Division: " + (num1 / num2));
-        System.out.println("Modulus: " + (num1 % num2));
-        System.out.println("\nRelational Operators ");
-        System.out.println("Equal to: " + (num1 == num2));
-        System.out.println("Not equal to: " + (num1 != num2));
-        System.out.println("Greater than: " + (num1 > num2));
-        System.out.println("Less than: " + (num1 < num2));
-        System.out.println("Greater than or equal to: " + (num1 >= num2));
-        System.out.println("Less than or equal to: " + (num1 <= num2
-        ));
-        System.out.println("\nLogical Operators ");
-        boolean a = true;
-        boolean b = false;
-        System.out.println("Logical AND: " + (a && b));
-        System.out.println("Logical OR: " + (a || b));
-        System.out.println("Logical NOT: " + (!a));
-        System.out.println("\nAssignment Operators ");
-        int c = 10;
-        c += 5; // c = c + 5
-        System.out.println("Addition Assignment: " + c);
-        c -= 3; // c = c - 3
-        System.out.println("Subtraction Assignment: " + c);
-        c *= 2; // c = c * 2
-        System.out.println("Multiplication Assignment: " + c);
-        c /= 4; // c = c / 4
-        System.out.println("Division Assignment: " + c);
-        c %= 3; // c = c % 3
-        System.out.println("Modulus Assignment: " + c);
+     //if else statement
+        int a = 10;
+        if(a > 5){
+            System.out.println("a is greater than 5");
+        } else {
+            System.out.println("a is less than or equal to 5");
+        }
+
+
+        // 3 variables if else elseif
+        int b = 20;
+        if(a > b){
+            System.out.println("a is greater than b");
+        } else if(a < b){
+            System.out.println("a is less than b");
+        } else {
+            System.out.println("a is equal to b");
+        }
+        // with && || operator
+        int c = 30;
+        if(a > b && a > c){
+            System.out.println("a is the greatest");
+        } else if(b > a && b > c){
+            System.out.println("b is the greatest");
+        } else if(c > a && c > b){
+            System.out.println("c is the greatest");
+        } else {
+            System.out.println("There is a tie");
+        }
+        
+
 
     } 
 
