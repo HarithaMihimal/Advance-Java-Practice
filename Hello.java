8050e69059856ed632063bdd7847422d6fdb194f
HarithaMihimal <harithamihimal4@gmail.com>
2026-06-16 07:36:14 +0530

    update Hello.java to demonstrate the new switch expression syntax in Java

--- Hello.java
+++ Hello.java
@@ -27,7 +27,18 @@ class Hello{
                 default:
                     System.out.println("Invalid day");
             }
-
+            // switch case new version
+            int day1 = 6;
+            switch (day1) {
+                case 1 -> System.out.println("Monday");
+                case 2 -> System.out.println("Tuesday");
+                case 3 -> System.out.println("Wednesday");
+                case 4 -> System.out.println("Thursday");
+                case 5 -> System.out.println("Friday");
+                case 6 -> System.out.println("Saturday");
+                case 7 -> System.out.println("Sunday");
+                default -> System.out.println("Invalid day");
+            }
 
     } 
 
