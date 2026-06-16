58ec2698cab3f6c78c7e29f54cf6ff4650d496d3
HarithaMihimal <harithamihimal4@gmail.com>
2026-06-16 07:39:39 +0530

    update Hello.java to demonstrate switch expressions with yield in Java

--- Hello.java
+++ Hello.java
@@ -40,6 +40,19 @@ class Hello{
                 default -> System.out.println("Invalid day");
             }
 
+            // switch with yield
+            int day2 = 2;
+            String dayName = switch (day2) {
+                case 1 : yield "Monday";
+                case 2 : yield "Tuesday";
+                case 3 : yield "Wednesday";
+                case 4 : yield "Thursday";
+                case 5 : yield "Friday";
+                case 6 : yield "Saturday";
+                case 7 : yield "Sunday";
+                default : yield "Invalid day";
+            };
+            System.out.println(dayName);
     } 
 
 }
